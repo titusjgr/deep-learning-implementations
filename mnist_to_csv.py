23286be5341abce3d822c55ef25f5baf0d937358
titusjgr <32384280+titusjgr@users.noreply.github.com>
2018-12-01 12:19:53 +0800

    Create mnist_to_csv.py
    This is the code I use to extract mnist training set and save them.

---
+++ mnist_to_csv.py
@@ -0,0 +1,12 @@
+from mnist import MNIST
+import pandas as pd
+data_path = '.'
+mndata = MNIST(data_path)
+mndata.gz = True
+train_X, train_Y = mndata.load_training()
+
+train_X = pd.DataFrame(train_X)
+train_Y = pd.Series(train_Y)
+
+train_X.to_csv('mnist_image.csv', index=False)
+train_Y.to_csv('mnist_label.csv', index=False)
