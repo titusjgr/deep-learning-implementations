b93508fa19eef906e41ba45b4774df820fdc6264
titusjgr <32384280+titusjgr@users.noreply.github.com>
2018-12-01 18:55:40 +0800

    I modified the df.to_csv parameters
    Now the csv only contains raw list data.

--- mnist-data/mnist_to_csv.py
+++ mnist-data/mnist_to_csv.py
@@ -8,5 +8,5 @@ train_X, train_Y = mndata.load_training()
 train_X = pd.DataFrame(train_X)
 train_Y = pd.Series(train_Y)
 
-train_X.to_csv('mnist_image.csv', index=False)
-train_Y.to_csv('mnist_label.csv', index=False)
+train_X.to_csv('mnist_image.csv', index=False, header=False)
+train_Y.to_csv('mnist_label.csv', index=False, header=False)
